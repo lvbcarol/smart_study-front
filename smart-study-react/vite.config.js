649f59ca8beeb9b869e4f675e8a00d9aa3751159
Carolina <carolinalansoni@gmail.com>
2025-07-24 16:33:18 -0300

    organização de arquivos e instalação de recharts e shadcn

--- smart-study-react/vite.config.js
+++ smart-study-react/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path" // Importa o 'path' do Node.js
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Adicione este bloco 'resolve' para o apelido funcionar
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
